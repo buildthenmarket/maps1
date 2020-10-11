82d2c8985eb81660c346397177160a1e5b9b627e
Savage CEO <savageceo@Savages-MacBook-Pro.local>
2020-10-11 15:39:55 -0400

    first commit

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,58 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { GoogleMap, GoogleMaps, GoogleMapsEvent, Marker, GoogleMapsAnimation, MyLocation } from '@ionic-native/google-maps';
+import { Platform } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page implements OnInit {
 
-  constructor() {}
+  map: GoogleMap;
+  address: string;
 
+  constructor(private platform: Platform) { }
+  ngOnInit() {
+    this.platform.ready();
+    this.loadMap();
+  }
+  loadMap() {
+    this.map = GoogleMaps.create('map_canvas', {
+      camera: {
+        target: {
+          lat: -33.867886,
+          lng: -63.987
+        },
+        zoom: 18
+      }
+    });
+    this.goToMyLocation();
+
+  }
+  goToMyLocation() {
+    this.map.clear();
+
+    this.map.getMyLocation().then((location: MyLocation) => {
+      console.log(JSON.stringify(location, null, 2));
+
+      this.map.animateCamera({
+        target: location.latLng,
+        zoom: 17,
+        duration: 5000
+      });
+
+      let marker: Marker = this.map.addMarkerSync({
+        title: 'Guitar',
+        snippet: 'Argentina',
+        position: location.latLng,
+        animation: GoogleMapsAnimation.BOUNCE
+      });
+
+      // marker.showInfoWindow();
+
+      // marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      //   this.s
+      // })
+    })
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
+
+declare var google: any;
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +9,24 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+  map: any;
+
+  @ViewChild('map', { read: ElementRef, static: false }) mapRef: ElementRef;
+
+  constructor() { }
+
+  ionViewDidEnter() {
+    this.showMap();
+  }
+
+  showMap() {
+    const location = new google.maps.LatLng(26.357896, 127.783809);
+    const options = {
+      center: location,
+      zoom: 15,
+      disableDefaultUI: true
+    }
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+  }
 
 }
